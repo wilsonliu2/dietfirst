3502f2e90be33f19dc2b4965923c229c7d2fd11a
wilson <wilsonliu202@gmail.com>
2024-12-08 22:23:27 -0500

    .

--- backend/app.js
+++ backend/app.js
@@ -22,7 +22,7 @@ const app = express();
 // CORS
 app.use(
   cors({
-    origin: "https://dietfirst-frontend.vercel.app",
+    origin: ["http://localhost:5173", "https://dietfirst-frontend.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
